dbdebd5763bacf479808b0db6bd07448dac1cb72
Pablo <pablorshds@gmail.com>
2026-01-21 19:59:44 -0300

    Adicionando alterações no httpService

--- src/app/services/backend/http.service.ts
+++ src/app/services/backend/http.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { RegisterDevice } from '../../interfaces/RegisterDevice';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { AllDevices } from '../../interfaces/AllDevices';
-import { AllSensors } from '../../interfaces/AllSensors';
-import { DeviceAnalysisDto } from '../../interfaces/DeviceAnalysisDto';
-import { Login } from '../../interfaces/Login';
-import { Notifications } from '../../interfaces/Notifications';
-import { RequestTokens } from '../../interfaces/RequestTokens';
-import { ResponseTokens } from '../../interfaces/ResponseTokens';
-import { UpdateDevice } from '../../interfaces/UpdateDevice';
+import { RegisterDevice } from '../../interfaces/device/RegisterDevice';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Device } from '../../interfaces/device/Device';
+import { Sensor } from '../../interfaces/sensor/Sensor';
+import { DeviceAnalysisDto } from '../../interfaces/analysis/DeviceAnalysisDto';
+import { Login } from '../../interfaces/login/Login';
+import { Notification } from '../../interfaces/notification/Notification';
+import { RequestTokens } from '../../interfaces/login/RequestTokens';
+import { ResponseTokens } from '../../interfaces/login/ResponseTokens';
+import { UpdateDevice } from '../../interfaces/device/UpdateDevice';
+import { UpdateSensor } from '../../interfaces/sensor/UpdateSensor';
+import { RequestUpdateDeviceAnalysis } from '../../interfaces/analysis/RequestUpdateDeviceAnalysis';
 
 @Injectable({
   providedIn: 'root'
@@ -18,24 +20,35 @@ export class HttpService {
 
   private http = inject(HttpClient);
 
+
   //DEVICE MANAGEMENT
-  register(device: RegisterDevice): Observable<AllDevices> {
-    return this.http.post<AllDevices>('http://localhost:8082/api/register-device', device);
+  register(device: RegisterDevice): Observable<Device> {
+    return this.http.post<Device>('http://localhost:8082/api/register-device', device);
   }
 
   //DEVICE MANAGEMENT
-  update(deviceModel: string ,device: UpdateDevice): Observable<AllDevices> {
-    return this.http.patch<AllDevices>(`http://localhost:8082/api/update-device/${deviceModel}`, device);
+  updateDevice(deviceModel: string ,device: UpdateDevice): Observable<Device> {
+    return this.http.patch<Device>(`http://localhost:8082/api/update-device/${deviceModel}`, device);
   }
 
   //DEVICE MANAGEMENT
-  delete(deviceModel: string): Observable<void> {
-    return this.http.delete<void>(`http://localhost:8082/api/delete-device/${deviceModel}`);
+  deleteDevice(deviceModel: string): Observable<Device> {
+    return this.http.delete<Device>(`http://localhost:8082/api/delete-device/${deviceModel}`);
   }
 
   //TEST
-  getAllSensorsActivated(): Observable<AllSensors[]> {
-    return this.http.get<AllSensors[]>('http://localhost:8083/api/find-all-sensors-activated');
+  updateSensor(deviceModel: string, updateSensor: UpdateSensor): Observable<Sensor> {
+    return this.http.patch<Sensor>(`http://localhost:8083/api/update-sensor/${deviceModel}`, updateSensor);
+  }
+
+  //TEST
+  deleteSensor(deviceModel: string): Observable<Sensor> {
+    return this.http.delete<Sensor>(`http://localhost:8083/api/delete-sensor/${deviceModel}`);
+  }
+
+  //TEST
+  getAllSensorsActivated(): Observable<Sensor[]> {
+    return this.http.get<Sensor[]>('http://localhost:8083/api/find-all-sensors-activated');
   }
 
   //TEST
@@ -44,23 +57,33 @@ export class HttpService {
   }
 
   //TEST
-  changeStatus(deviceModel: string): Observable<void> {
-    return this.http.patch<void>(`http://localhost:8083/api/change-status/${deviceModel}`, deviceModel);
+  changeStatus(deviceModel: string): Observable<Sensor> {
+    return this.http.patch<Sensor>(`http://localhost:8083/api/change-status/${deviceModel}`, deviceModel);
   }
 
   //DEVICE MANAGEMENT
-  allDevices(): Observable<AllDevices[]> {
-    return this.http.get<AllDevices[]>('http://localhost:8082/api/all-devices');
+  getAllDevices(): Observable<Device[]> {
+    return this.http.get<Device[]>('http://localhost:8082/api/all-devices');
   }
 
   //DEVICE MANAGEMENT
-  findDeviceWithDeviceModel(deviceModel: string): Observable<AllDevices> {
-    return this.http.get<AllDevices>(`http://localhost:8082/api/find-by-device/${deviceModel}`);
+  findDeviceWithDeviceModel(deviceModel: string): Observable<Device> {
+    return this.http.get<Device>(`http://localhost:8082/api/find-by-device/${deviceModel}`);
   }
 
   //ANALYSIS
-  findDeviceModelForAnalysis(deviceModel: string): Observable<DeviceAnalysisDto> {
+  getDeviceAnalysisForModel(deviceModel: string): Observable<DeviceAnalysisDto> {
     return this.http.get<DeviceAnalysisDto>('http://localhost:8084/api/get-device-for-model', { params: { deviceModel } });
+  }
+
+  //ANALYSIS
+  updateAnalysis(deviceModel: string, request: RequestUpdateDeviceAnalysis): Observable<DeviceAnalysisDto> {
+    return this.http.patch<DeviceAnalysisDto>(`http://localhost:8084/api/update-analysis/${deviceModel}`, request);
+  }
+
+  //ANALYSIS
+  deleteAnalysis(deviceModel: string): Observable<DeviceAnalysisDto> {
+    return this.http.delete<DeviceAnalysisDto>(`http://localhost:8084/api/delete-analysis/${deviceModel}`);
   }
 
   //LOGIN
@@ -100,8 +123,8 @@ export class HttpService {
   }
 
   //NOTIFICAÇÃO
-  notifications(page: number, size: number):Observable<Notifications[]>{
-    return this.http.get<Notifications[]>(`http://localhost:8085/api/notifications?page=${page}&size=${size}`);
+  notifications(page: number, size: number):Observable<Notification[]>{
+    return this.http.get<Notification[]>(`http://localhost:8085/api/notifications?page=${page}&size=${size}`);
   }
 
   //NOTIFICAÇÃO
