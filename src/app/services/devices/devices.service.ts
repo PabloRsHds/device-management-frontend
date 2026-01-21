3c5161db20e3572b2ef8956add6886f760bfca37
Pablo <pablorshds@gmail.com>
2026-01-21 20:01:58 -0300

    Adcionando alteração no serviço Devices

--- src/app/services/devices/devices.service.ts
+++ src/app/services/devices/devices.service.ts
@@ -1,23 +1,15 @@
 import { inject, Injectable } from '@angular/core';
-import { DevicesStateService } from '../states/devices-state.service';
-import { AllDevices } from '../../interfaces/AllDevices';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { DevicesStateService } from '../state/devices/devices-state.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevicesService {
 
-  private state = inject(DevicesStateService)
+  private state = inject(DevicesStateService);
 
-  submitGetDevices() : Observable<AllDevices[]> {
-    return this.state.addDevicesToState().pipe(
-      tap(),
-      catchError((error) => {
-        console.error('Error registering device:', error);
-        return throwError(() => error)
-      } )
-    );
+  getAllDevices() {
+    return this.state.getAllDevices();
   }
 
 }
